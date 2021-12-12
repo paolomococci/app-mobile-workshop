7c2c79c27d0e86ead84195ee941bbbc6464c0fc4
paolomococci <paolomococci@gmail.com>
2021-12-12 04:30:59 +0100

    modified file main.dart belonging to directory closures

--- dart_examples/closures/main.dart
+++ dart_examples/closures/main.dart
@@ -1,1 +1,24 @@
-void main(List<String> args) {}
+void main() {
+  consumeClosure();
+}
+
+void callbackExample(void callback(String value)) {
+  callback('Hello from callback function!');
+}
+
+typedef NumberGetter = num Function();
+
+num powerOfTwo(NumberGetter getter) {
+  return getter() * getter();
+}
+
+void consumeClosure() {
+  final getTwentyFour = () => 6 / (1 - 3 / 4);
+  final fourSquared = powerOfTwo(getTwentyFour);
+
+  print(fourSquared);
+
+  callbackExample((result) {
+    print(result);
+  });
+}
