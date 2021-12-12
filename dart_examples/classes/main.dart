7eaab3123f8e50aea8cbf92a150ff8523636d65d
paolomococci <paolomococci@gmail.com>
2021-12-12 04:32:03 +0100

    modified file main.dart belonging to directory classes

--- dart_examples/classes/main.dart
+++ dart_examples/classes/main.dart
@@ -1,1 +1,33 @@
-void main(List<String> args) {}
+void main() {
+  classesPlayground();
+}
+
+void classesPlayground() {
+  final name = OfficialName('Mr', 'John', 'Doe');
+  final result = name.toString();
+
+  print(result);
+}
+
+class Name {
+  final String first;
+  final String last;
+
+  Name(this.first, this.last);
+
+  String toString() {
+    return '$first $last';
+  }
+}
+
+class OfficialName extends Name {
+  /* private properties begin with an underscore */
+  final String _title;
+
+  OfficialName(this._title, String first, String last) : super(first, last);
+
+  @override
+  String toString() {
+    return '$_title $first $last';
+  }
+}
