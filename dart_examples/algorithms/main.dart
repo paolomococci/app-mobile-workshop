408ae555cbd7630225b7164b38780b5718ee56e3
paolomococci <paolomococci@gmail.com>
2021-12-13 05:45:03 +0100

    modified file main.dart belonging to directory algorithms

--- dart_examples/algorithms/main.dart
+++ dart_examples/algorithms/main.dart
@@ -1,5 +1,36 @@
 import 'person.dart';
 
+List<Map> data = [
+  /* names, surnames and ages invented for the sole purpose of example */
+  {'name': 'Alan', 'surname': 'Dwell', 'age': 23},
+  {'name': 'Mathew', 'surname': 'Burn', 'age': 22},
+  {'name': 'Gillian', 'surname': 'Emit', 'age': 24},
+  {'name': 'Susan', 'surname': 'Nourish', 'age': 21},
+  {'name': 'Hannah', 'surname': 'Zoom', 'age': 22},
+  {'name': 'Mary', 'surname': 'Jab', 'age': 23},
+  {'name': 'Francis', 'surname': 'Yield', 'age': 21},
+  {'name': 'Julie', 'surname': 'Wring', 'age': 21},
+  {'name': 'Steve', 'surname': 'Keep', 'age': 22},
+  {'name': 'Helen', 'surname': 'Verse', 'age': 23},
+  {'name': 'Amy', 'surname': 'Weep', 'age': 21},
+  {'name': 'Charles', 'surname': 'Quit', 'age': 22},
+  {'name': 'Fiona', 'surname': 'Take', 'age': 21},
+  {'name': 'Eric', 'surname': 'Leap', 'age': 22},
+  {'name': 'Joan', 'surname': 'Ooze', 'age': 24},
+  {'name': 'Sophie', 'surname': 'Ring', 'age': 25},
+  {'name': 'Ian', 'surname': 'Plead', 'age': 21},
+  {'name': 'Keith', 'surname': 'Dive', 'age': 21},
+  {'name': 'Roger', 'surname': 'Spill', 'age': 22},
+  {'name': 'Clare', 'surname': 'Gild', 'age': 23},
+  {'name': 'Alice', 'surname': 'Hound', 'age': 22},
+  {'name': 'Victoria', 'surname': 'Mow', 'age': 22},
+  {'name': 'Jonathan', 'surname': 'Xmas', 'age': 21},
+  {'name': 'Marion', 'surname': 'Arise', 'age': 23},
+  {'name': 'Sheila', 'surname': 'Naive', 'age': 22},
+  {'name': 'Tony', 'surname': 'Cast', 'age': 21},
+  {'name': 'Mark', 'surname': 'Flee', 'age': 21},
+];
+
 void main() {
   classPlayground();
 }
