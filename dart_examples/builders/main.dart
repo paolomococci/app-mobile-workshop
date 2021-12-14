5ae4497587d76363b76bd24d8f9bd13c981eea78
paolomococci <paolomococci@gmail.com>
2021-12-14 18:20:21 +0100

    modified file main.dart belonging to the directory builders

--- dart_examples/builders/main.dart
+++ dart_examples/builders/main.dart
@@ -1,1 +1,30 @@
-void main(List<String> args) {}
+import 'builder.dart';
+
+void main() {
+  builderPlayground();
+  cascadePlayground();
+}
+
+void builderPlayground() {
+  final url = UriBuilder()
+    ..protocol = 'http'
+    ..host = 'sample.examples.local'
+    ..routes = [
+      'home',
+      'help',
+      'some#1',
+    ];
+
+  print('');
+  print(url);
+}
+
+void cascadePlayground() {
+  final numberVector = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    ..insert(0, 2)
+    ..sort((a, b) => a.compareTo(b));
+
+  print('');
+  print(
+      'The largest prime number of this array: $numberVector, is: ${numberVector.last}.');
+}
