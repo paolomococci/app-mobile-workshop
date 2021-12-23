99c2d765229102d754612066b616643d1b26c177
paolomococci <paolomococci@gmail.com>
2021-12-23 16:09:08 +0100

    edited file main.dart belonging to artifact project

--- artifact/lib/main.dart
+++ artifact/lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Artifact',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Artifact Demo App'),
     );
   }
 }
